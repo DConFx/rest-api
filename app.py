e0d0d1ba3b92b27f946089d8a3fcb80de6f59591
DConFX <danielcirdei@gmail.com>
2025-03-17 16:12:22 +0000

    Update to app

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def health_check():
 
 @app.route('/hello')
 def hello():
-    return jsonify({"message": "Hello, World!"}), 200
+    return jsonify({"message": "Hello, World! We are here with good news!"}), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
